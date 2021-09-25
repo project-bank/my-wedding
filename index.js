3701c8d177d456a11795497c2cfeffa1afe59148
Agung Kurniawan <agung.k@gdn-commerce.com>
2021-09-25 22:14:44 +0700

    foking random tutorial give default port 5000

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express'); //Import the express dependency
 const app = express();              //Instantiate an express app, the main work horse of this server
-const port = 5000;                  //Save the port number where your server will be listening
 
-app.use('/',express.static(__dirname));
+app.use(express.static("public"))
 
 //Idiomatic expression in express to route and respond to a client request
 app.get('/', (req, res) => {        //get requests to the root ("/") will route here
@@ -10,6 +9,7 @@ app.get('/', (req, res) => {        //get requests to the root ("/") will route 
                                                         //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
 });
 
+const port = process.env.PORT || 3000
 app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
     console.log(`Now listening on port ${port}`); 
 });
